3b2be16403a5ef3298dcadf4da0e83ab6a1ea267
dayinta84 <dayintaayu48@gmail.com>
2024-12-02 08:46:14 +0700

    home

--- app/src/main/java/com/example/glossaryy/Home.kt
+++ app/src/main/java/com/example/glossaryy/Home.kt
@@ -8,17 +8,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -42,99 +44,145 @@ class Home : ComponentActivity() {
 fun MainScreen() {
     val context = LocalContext.current
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(
-                brush = Brush.verticalGradient(
-                    colors = listOf(Color(0xFF381E72), Color(0xFFFFFFEE))
+    Scaffold(
+        bottomBar = { HomeBottomNavigationBar() } // Navbar tetap di bagian bawah
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding) // Mencegah konten tertutup navbar
+                .verticalScroll(rememberScrollState())
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(Color(0xFF381E72), Color(0xFFFFFFEE))
+                    )
                 )
-            )
-            .padding(16.dp)
-    ) {
-        Text(
-            text = "GLOSSARY",
-            fontWeight = FontWeight.Bold,
-            fontSize = 35.sp,
-            color = Color.White
-        )
-        Spacer(modifier = Modifier.height(80.dp))
-        Card(
-            shape = RoundedCornerShape(30.dp),
-            elevation = CardDefaults.cardElevation(4.dp),
-            modifier = Modifier.fillMaxWidth()
+                .padding(16.dp)
         ) {
-            Column(
-                modifier = Modifier.padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
+            Text(
+                text = "GLOSSARY",
+                fontWeight = FontWeight.Bold,
+                fontSize = 35.sp,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(80.dp))
 
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Spacer(modifier = Modifier.width(230.dp))
+            Card(
+                shape = RoundedCornerShape(30.dp),
+                elevation = CardDefaults.cardElevation(4.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        val intent = Intent(context, ProfilActivity::class.java)
+                        context.startActivity(intent)
+                    }
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Spacer(modifier = Modifier.weight(1f))
+                        Text(
+                            text = "Hi, Elda",
+                            fontSize = 18.sp
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Image(
+                            painter = painterResource(id = R.drawable.wajah),
+                            contentDescription = "Profil",
+                            modifier = Modifier
+                                .size(30.dp)
+                                .clickable {
+                                    val intent = Intent(context, ProfilActivity::class.java)
+                                    context.startActivity(intent)
+                                }
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
                     Text(
-                        text = "Hai, Elda",
+                        text = "Point",
                         fontSize = 18.sp
                     )
-                    Image(
-                        painter = painterResource(id = R.drawable.wajah),
-                        contentDescription = "Profil",
-                        modifier = Modifier.size(30.dp)
-                    )
-                }
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    text = "Point",
-                    fontSize = 18.sp
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Card(
-                    shape = RoundedCornerShape(10.dp),
-                    elevation = CardDefaults.cardElevation(2.dp),
-                    modifier = Modifier.padding(8.dp)
-                ) {
-                    Text(
-                        text = "5900",
-                        fontSize = 36.sp,
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Card(
+                        shape = RoundedCornerShape(10.dp),
+                        elevation = CardDefaults.cardElevation(2.dp),
                         modifier = Modifier.padding(8.dp)
-                    )
+                    ) {
+                        Text(
+                            text = "5900",
+                            fontSize = 36.sp,
+                            modifier = Modifier.padding(8.dp)
+                        )
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Kategori Kuis",
+                fontSize = 18.sp,
+                color = Color(0xFFD3D3D3)
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+
+            /*Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                CategoryCard("Level 1", R.drawable.level1) {
+                    val intent = Intent(context, QuizSD::class.java)
+                    context.startActivity(intent)
+                }
+                CategoryCard("Level 2", R.drawable.level2) {
+                    // Navigasi ke halaman kuis level 2
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                CategoryCard("Level 3", R.drawable.level3) {
+                    // Navigasi ke halaman kuis level 3
+                }
+                CategoryCard("Level 4", R.drawable.level4) {
+                    // Navigasi ke halaman kuis level 4
+                }
+            }*/
+
+            // Grid Kategori
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(8.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                item {
+                    CategoryCard("Level 1", R.drawable.level1) {
+                        val intent = Intent(context, QuizSD::class.java)
+                        context.startActivity(intent)
+                    }
+                }
+                item {
+                    CategoryCard("Level 2", R.drawable.level2) {
+                        // Navigasi ke halaman kuis level 2
+                    }
+                }
+                item {
+                    CategoryCard("Level 3", R.drawable.level3) {
+                        // Navigasi ke halaman kuis level 3
+                    }
+                }
+                item {
+                    CategoryCard("Level 4", R.drawable.level4) {
+                        // Navigasi ke halaman kuis level 4
+                    }
                 }
             }
         }
-
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(
-            text = "Kategori Kuis",
-            fontSize = 18.sp,
-            color = Color(0xFFD3D3D3)
-        )
-        Spacer(modifier = Modifier.height(30.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            CategoryCard("Level 1", R.drawable.level1) {
-                // Intent untuk berpindah ke QuizSD
-                val intent = Intent(context, QuizSD::class.java)
-                context.startActivity(intent)
-            }
-            CategoryCard("Level 2", R.drawable.level2) {
-                // Implementasi untuk Level 2 jika diperlukan
-            }
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            CategoryCard("Level 3", R.drawable.level3) {
-                // Implementasi untuk Level 3 jika diperlukan
-            }
-            CategoryCard("Level 4", R.drawable.level4) {
-                // Implementasi untuk Level 4 jika diperlukan
-            }
-        }
-        Spacer(modifier = Modifier.weight(1f))
-        HomeBottomNavigationBar()
     }
 }
 
@@ -146,34 +194,34 @@ fun CategoryCard(title: String, imageRes: Int, onClick: () -> Unit) {
         modifier = Modifier
             .width(185.dp)
             .height(160.dp)
-            .clickable { onClick() }  // Menambahkan clickable modifier
+            .clickable { onClick() }
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
             modifier = Modifier
-                .fillMaxSize()  // Menjamin Column mengisi seluruh ukuran Card
-                .padding(20.dp) // Menambahkan padding jika diperlukan
+                .fillMaxSize()
+                .padding(20.dp)
         ) {
             Image(
                 painter = painterResource(id = imageRes),
                 contentDescription = title,
                 modifier = Modifier.size(85.dp)
             )
-            Spacer(modifier = Modifier.height(8.dp))  // Memberi jarak antara gambar dan teks
+            Spacer(modifier = Modifier.height(8.dp))
             Text(
                 text = title,
-                fontSize = 20.sp,  // Menyesuaikan ukuran font untuk tampil lebih baik
+                fontSize = 20.sp,
                 fontWeight = FontWeight.Bold
             )
         }
     }
 }
 
-
-
 @Composable
 fun HomeBottomNavigationBar() {
+    val context = LocalContext.current
+
     BottomAppBar(
         containerColor = Color(0xFF3C0CA6),
         contentColor = Color.White,
@@ -184,25 +232,37 @@ fun HomeBottomNavigationBar() {
             horizontalArrangement = Arrangement.SpaceAround,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = {
+                val intent = Intent(context, Home::class.java)
+                context.startActivity(intent)
+            }) {
                 Icon(
                     painter = painterResource(id = R.drawable.home),
                     contentDescription = "Home"
                 )
             }
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = {
+                val intent = Intent(context, PeringkatActivity::class.java)
+                context.startActivity(intent)
+            }) {
                 Icon(
                     painter = painterResource(id = R.drawable.tropy),
                     contentDescription = "Trophy"
                 )
             }
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = {
+                val intent = Intent(context, QuizHistoryActivity::class.java)
+                context.startActivity(intent)
+            }) {
                 Icon(
                     painter = painterResource(id = R.drawable.simpan),
                     contentDescription = "Bookmark"
                 )
             }
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = {
+                val intent = Intent(context, ProfilActivity::class.java)
+                context.startActivity(intent)
+            }) {
                 Icon(
                     painter = painterResource(id = R.drawable.user),
                     contentDescription = "User"
@@ -211,4 +271,3 @@ fun HomeBottomNavigationBar() {
         }
     }
 }
-
